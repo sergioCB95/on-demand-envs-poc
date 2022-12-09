b7f364a47e7422d0c0e21174f74f7b316716943e
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2022-12-09 17:26:36 +0100

    fix: fixes in makefile and user-data

--- infra/back/main.tf
+++ infra/back/main.tf
@@ -34,8 +34,8 @@ module "ec2-backend" {
 
   ami = "ami-076309742d466ad69"
   app_dir = "back"
-  app_name = "on-demand-envs-poc"
-  app_port = "4000"
+  app_name = "${terraform.workspace}-on-demand-envs-poc"
+  app_port = "3000"
   vpc_id = data.terraform_remote_state.core.outputs.vpc_id
-  vpc_subnet = data.terraform_remote_state.core.outputs.vpc_private_subnet_1_id
+  vpc_subnet = data.terraform_remote_state.core.outputs.vpc_public_subnet_1_id
 }
--- infra/commons/ec2-node/user-data/user-data.sh
+++ infra/commons/ec2-node/user-data/user-data.sh
@@ -6,10 +6,15 @@ echo "installing git"
 apt-get install -y git
 /usr/bin/yum install -y git
 
+echo "installing NodeJS"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
+yum -y install nodejs
+
+
 echo "cloning repo"
 mkdir /app
 git clone "https://github.com/sergioCB95/on-demand-envs-poc.git" /app
-cd /app/$APP_DIR
+cd /app/${APP_DIR}
 
 echo "installing npm packages"
 make install
